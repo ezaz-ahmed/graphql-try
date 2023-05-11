8aa084bc81c5feddb2a1a4b8e4dd54fbe90a80d7
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-05-11 10:53:32 +0600

    Movie Create

--- server/scehma/resolvers.js
+++ server/scehma/resolvers.js
@@ -1,8 +1,10 @@
-import { UserList } from '../FakeData.js';
+import { MovieList, UserList } from '../FakeData.js';
 
 export const resolvers = {
   Query: {
     users: () => UserList,
     user: (_, args) => UserList.find(user => user.id === Number(args.id)),
+    movies: () => MovieList,
+    movie: (_, args) => MovieList.find(movie => movie.name === args.name),
   },
 };
